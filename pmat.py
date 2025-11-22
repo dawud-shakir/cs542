20241b4cbfebf47850f181f83cab33862e35fd55
dawud-shakir <dsshakir@gmail.com>
2025-11-22 13:50:10 -0700

    File I/O; Simple timing; Early stopping conditions

--- pmat.py
+++ pmat.py
@@ -81,6 +81,97 @@ def reset_text_color():
 
 class pmat:
     ############################################################################
+    # File I/O
+    ############################################################################
+    def to_file(self, filename: str):
+        comm = MPI.COMM_WORLD
+        # Open the file
+        amode = MPI.MODE_CREATE | MPI.MODE_WRONLY
+        fh = MPI.File.Open(comm, filename, amode)
+
+        data_offset = 0
+
+        # Write header
+        if self.rank == 0:
+            dtype_str = np.dtype(self.dtype).name  # e.g., 'float64'
+            dtype_bytes = dtype_str.encode('utf-8')
+            dtype_len = np.int32(len(dtype_bytes))  # store length as 4 bytes
+
+            header = np.array([self.n, self.m], dtype=np.int64).tobytes()
+            fh.Write_at(0, header)
+            fh.Write_at(16, dtype_len.tobytes())
+            fh.Write_at(20, dtype_bytes)
+            
+            # n (8 bytes), m (8 bytes), dtype_len (4 bytes), dtype_str (dtype_len)
+            
+            data_offset += 20 + dtype_len
+
+        data_offset = np.array(data_offset, dtype=np.int64)
+        MPI.COMM_WORLD.Bcast(data_offset, root=0)
+        data_offset = int(data_offset)  # Convert back to int
+        
+        # Wait for header to be written before writing any data
+        comm.Barrier()  
+        
+        coords = self.coords
+        extent = self.extents[coords[0]][coords[1]]
+        offset = self.offsets[coords[0]][coords[1]]
+
+        local_rows, local_cols = extent[0], extent[1]
+        row_offset, col_offset = offset[0], offset[1]
+
+        for i in range(local_rows):
+            global_row = row_offset + i
+            file_offset = data_offset + (global_row * self.m + col_offset) * np.dtype(self.dtype).itemsize
+            fh.Write_at(file_offset, self.local[i, :local_cols].tobytes())
+
+        fh.Close()
+
+    @staticmethod
+    def from_numpy(filename: str) -> 'pmat':
+        comm = MPI.COMM_WORLD
+        # Open the file
+        amode = MPI.MODE_RDONLY
+        fh = MPI.File.Open(comm, filename, amode)
+
+        # file_size = fh.Get_size()
+
+        header = np.empty(2, dtype=np.int64)
+
+        fh.Read_at(0, header)
+        dtype_len = np.empty(1, dtype=np.int32)
+        fh.Read_at(16, dtype_len)
+        dtype_bytes = bytearray(dtype_len[0])
+        fh.Read_at(20, dtype_bytes)
+        dtype_str = dtype_bytes.decode('utf-8')
+        
+        dtype = np.dtype(dtype_str)
+        nrows, ncols = header
+
+        data_offset = 20 + dtype_len[0]
+        
+        # Empty pmat
+        pmat_matrix = pmat(nrows, ncols)
+
+        coords = pmat_matrix.coords
+        extent = pmat_matrix.extents[coords[0]][coords[1]]
+        offset = pmat_matrix.offsets[coords[0]][coords[1]]
+
+        local_rows, local_cols = extent[0], extent[1]
+        row_offset, col_offset = offset[0], offset[1]
+
+        for i in range(local_rows):
+            global_row = row_offset + i
+            file_offset = data_offset + (global_row * ncols + col_offset) * np.dtype(dtype).itemsize
+            buffer = bytearray(local_cols * np.dtype(dtype).itemsize)
+            fh.Read_at(file_offset, buffer)
+            pmat_matrix.local[i, :local_cols] = np.frombuffer(buffer, dtype=dtype)
+
+        fh.Close()
+        return pmat_matrix
+
+
+    ############################################################################
     # Static members and methods
     ############################################################################
 
